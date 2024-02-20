23248638f883b8564d7814e8e865b82c5c77bf78
ghamlet <dmtr317744@gmail.com>
2024-02-21 02:32:48 +0300

    kl

--- BaseCode/trackbars.py
+++ BaseCode/trackbars.py
@@ -2,12 +2,7 @@ import cv2
 import os
 import numpy as np
 
-minblue, mingreen, minred, maxblue, maxgreen, maxred = 151, 255, 0, 255, 255, 255
-
-#minblue, mingreen, minred, maxblue, maxgreen, maxred = 0,0,0,0,0,0
-
-pos = 0 
-
+minblue, mingreen, minred, maxblue, maxgreen, maxred = 0, 0, 0, 255, 255, 255
 
 change_image = False
 
@@ -34,19 +29,20 @@ def trackbar(minblue=0, mingreen=0, minred=0, maxblue=0, maxgreen=0, maxred=0):
     change_image = True
 
 
-number_of_files = len(os.listdir(dir_images))
-print(f"Всего файлов {number_of_files}")
-
+cap = cv2.VideoCapture("BaseCode/video_2024-02-21_01-35-52.mp4")
 
 while True:
-    
+    res, frame_input =  cap.read()
+    if not res:
+        break
+
     if change_image == False:
         trackbar(minblue, mingreen, minred, maxblue, maxgreen, maxred) #create trackbars
     
         
-    img = os.listdir(dir_images)[pos]
-    frame_input = f"foto/{img}"
-    frame_input = cv2.imread(frame_input)
+    # img = os.listdir(dir_images)[pos]
+    # frame_input = f"foto/{img}"
+    # frame_input = cv2.imread(frame_input)
 
     hsv = cv2.cvtColor(frame_input, cv2.COLOR_BGR2HSV)
     color_tone_input = hsv[:,:,0]
@@ -65,49 +61,23 @@ while True:
 
     result=cv2.bitwise_and(frame_input,frame_input,mask=mask)
 
-    result = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
-    color_tone_result = result[:,:,0]
-    color_tone_result = np.sum(color_tone_result)
+    # result = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
+    # color_tone_result = result[:,:,0]
+    # color_tone_result = np.sum(color_tone_result)
 
-    proportion = int((color_tone_result / color_tone_input) * 1000)
-    print(proportion)
+    # proportion = int((color_tone_result / color_tone_input) * 1000)
+    # print(proportion)
 
-    if proportion in range(80, 1200):
-        print("stop")
-    else:
-        print("turn ")
+    # if proportion in range(80, 1200):
+    #     print("stop")
+    # else:
+    #     print("turn ")
 
     cv2.imshow('result1', result)
     cv2.imshow('frame_input', frame_input)
     cv2.imshow('mask', mask)
+    k = cv2.waitKey(100)
+    if k == ord('q'):
+        break
     
     
-    
-    k = cv2.waitKey(1)
-
-    if k == ord('a'):
-        pos=pos-1
-
-        if pos < 0:
-            pos = 0
-            print("the first file is open")
-            
-        else:
-            minblue, mingreen, minred, maxblue, maxgreen, maxred = minb, ming, minr, maxb, maxg, maxr
-            change_image = False
-            cv2.destroyAllWindows()
-       
-       
-       
-    elif k == ord('d'):
-
-        pos=pos+1
-
-        if pos == number_of_files:
-            pos = number_of_files - 1
-            print("files are over")
-
-        else:
-            minblue, mingreen, minred, maxblue, maxgreen, maxred = minb, ming, minr, maxb, maxg, maxr
-            change_image = False
-            cv2.destroyAllWindows()
