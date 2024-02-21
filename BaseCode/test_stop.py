c35d5484fe19ffd7750982cc28a694905bc7deb8
ghamlet <dmtr317744@gmail.com>
2024-02-21 13:01:23 +0300

    hjk

--- BaseCode/test_stop.py
+++ BaseCode/test_stop.py
@@ -1,13 +1,39 @@
-# import cv2
-# cap = cv2.VideoCapture("source_from_nto/BaseCode/area.mp4")
-# while True:
-#     # start_time = time.time()
-#     ret, frame = cap.read()
-#     hsv = cv2.cvtColor(frame_input, cv2.COLOR_BGR2HSV)
+import cv2
+import numpy as np
 
-#     # end_frame = time.time()
-#     if not ret:
-#         break
+cap = cv2.VideoCapture("source_from_nto/BaseCode/area.mp4")
+minb, ming, minr, maxb, maxg, maxr = 22, 67, 96, 255, 255, 255
+AREA  = False
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv,(minb,ming,minr),(maxb,maxg,maxr))
+
+    hist = np.sum(mask, axis=1)
+    maxStrInd = np.argmax(hist)
+    print(hist[maxStrInd]//255)
+    #hist = np.sum(mask)
+    # print(hist)
+    # if hist > 28303725:
+    #     AREA = True
+
+    result = cv2.bitwise_and(frame,frame,mask=mask)
+
+    # maxStrInd = np.argmax(hist)
+    # print(maxStrInd)
+
+
+   
+
+    cv2.imshow('result1', result)
+    cv2.imshow('frame_input', frame)
+    cv2.imshow('mask', mask)
+    k = cv2.waitKey(10)
+    if k == ord('q'):
+        break
+   
     
-#     cv2.imshow("frame", frame)
-#     cv2.waitKey(10)
+  
